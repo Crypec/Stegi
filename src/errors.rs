43cb6a7678f5a6cff98fad27281b331056aace4b
Simon Kunz <simon.kunz21@gmail.com>
2020-03-28 13:09:43 +0100

    add errors for parsing

--- src/errors.rs
+++ src/errors.rs
@@ -8,4 +8,18 @@ pub enum SyntaxError {
 
     #[fail(display = "Syntaxfehler: Textliteral nicht geschlosssen '{}'", _0)]
     UnterminatedString(usize),
+
+    #[fail(display = "SyntaxFehler: Unerwartetes Dateiende")]
+    UnexpectedEOF,
+
+    #[fail(
+        display = "SyntaxFehler: Nach {} haben wir eigentlich {} erwartet",
+        _0, _1
+    )]
+    Missing(String, &'static str),
+
+    #[fail(
+        display = "SyntaxFehler: Du scheinst den 'Stop' Befehl ausserhalb einer Schleife benutzt zu haben"
+    )]
+    BreakOutsideLoop,
 }
