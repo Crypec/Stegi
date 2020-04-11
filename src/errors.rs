9d1c22d65b33cfc66a3db35db449bc15fb145f77
Simon Kunz <simon.kunz21@gmail.com>
2020-04-11 23:29:54 +0200

    implement display for diagnostic properly

--- src/errors.rs
+++ src/errors.rs
@@ -1,8 +1,7 @@
 use std::fmt;
-use std::fs;
+use std::rc::Weak;
 
 use crate::ast::Span;
-use crate::lexer::*;
 use crate::session::*;
 
 use colored::*;
@@ -44,100 +43,142 @@ pub enum Severity {
     CodeRed, // Reserved for only the highest severity alarms, this means we fucked something up :D
 }
 
-pub struct Diagnostic<'a> {
-    pub msg: String,
+// TODO(Simon): do some better error handling with the weak ref to parent session
+pub struct Diagnostic {
     pub desc: String,
+    pub msg: String,
     pub suggestions: Vec<String>,
     pub severity: Severity,
     pub span: Span,
-    pub src: &'a SourceMap,
+    pub src: Weak<SourceMap>,
 }
 
-impl Diagnostic<'_> {
-    fn line_number(&self) -> usize {
-        // FIXME(Simon): I haven't tested this, but it seems like this is going to work only on linux with the current solution
-        // FIXME(Simon): because windows uses not only a '\n' as a newline char but combines it with a '\r'
-        fs::read_to_string(&self.src.path)
-            .expect("failed to read file")
+impl Diagnostic {
+    pub fn new<S: Into<String>>(
+        desc: S,
+        msg: S,
+        suggestions: Vec<S>,
+        severity: Severity,
+        span: Span,
+        src: Weak<SourceMap>,
+    ) -> Self {
+        Self {
+            desc: desc.into(),
+            msg: msg.into(),
+            suggestions: suggestions
+                .into_iter()
+                .map(|s| s.into())
+                .collect::<Vec<String>>(),
+            severity,
+            span: span.clone(),
+            src,
+        }
+    }
+
+    pub fn emit(&self) {
+        println!("{}", self);
+    }
+
+    fn line_num(&self) -> usize {
+        self.src
+            .upgrade()
+            .unwrap()
+            .buf
             .chars()
-            .take(self.span.lo)
             .filter(|c| *c == '\n')
             .count()
     }
 
-    fn line_span(&self) -> Span {
-        let src = fs::read_to_string(&self.src.path).unwrap();
-        let (lo, _) = src
-            .char_indices()
-            .filter(|(_, c)| *c == '\n')
-            .nth(self.line_number())
-            .unwrap();
-        dbg!(self.line_span());
-        let (hi, _) = src
-            .char_indices()
-            .skip(lo + 1)
-            .find(|(_, c)| *c == '\n')
-            .unwrap();
-        let res = Span::new(lo, hi);
-        res
-    }
-
-    fn fmt_src_line(&self) -> String {
-        format!(
-            "|{} {}\n|   {}",
-            self.line_number(),
-            self.get_src_line(),
-            self.underline()
-        )
-    }
-
     fn underline(&self) -> String {
         let line_span = self.line_span();
-        let file = fs::read_to_string(&self.src.path).unwrap();
-        dbg!(line_span);
-        dbg!(self.span);
-        let buf_len = self.span.lo - line_span.lo;
-        let buf = (0..buf_len).map(|_| " ").collect::<String>();
-        let under = (0..self.span.hi - self.span.lo + 1)
-            .map(|_| "^".red().bold().to_string())
-            .collect::<String>();
-        format!("{}{}", buf, under)
+        let buf_len = line_span.hi - self.span.lo;
+        (1..buf_len).map(|_| "^").collect::<String>()
     }
 
-    fn get_src_line(&self) -> String {
-        let line = self.line_span();
-        fs::read_to_string(&self.src.path).unwrap()[line.lo..line.hi]
-            .trim_start_matches("\n")
-            .to_owned()
+    fn span_snippet(&self) -> String {
+        let s = self.line_span();
+        self.src.upgrade().unwrap().buf[s.lo..s.hi]
+            .trim_start()
+            .to_string()
     }
 
-    pub fn suggest(&mut self, sug: &str) {
-        self.suggestions.push(sug.to_owned());
+    fn line_span(&self) -> Span {
+        // FIXME(Simon): I haven't tested this, but it seems like this is going to work only on linux with the current solution
+        // FIXME(Simon): because windows uses not only a '\n' as a newline char but combines it with a '\r'
+        let mut line_offsets = vec![0];
+        line_offsets.extend(
+            &self
+                .src
+                .upgrade()
+                .unwrap()
+                .buf
+                .char_indices()
+                .filter(|(_, c)| *c == '\n')
+                .map(|(i, _)| i)
+                .collect::<Vec<usize>>(),
+        );
+        assert!(
+            self.span.hi <= *line_offsets.last().unwrap(),
+            "`hi` marker of span is outside of file"
+        );
+
+        let mut it = line_offsets.into_iter().peekable();
+        let mut lo = 0;
+        while let Some(offset) = it.next() {
+            if offset == self.span.lo || *it.peek().unwrap() >= self.span.lo {
+                lo = offset;
+                break;
+            }
+        }
+        let hi = it.next().unwrap();
+        // we add 1 to the low marker of the line span to skip the `\n = newline` char
+        Span::new(lo + 1, hi)
     }
 }
 
-impl fmt::Display for Diagnostic<'_> {
+impl fmt::Display for Diagnostic {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let header = format!(
-            "-- {} --------------------------------------- [{}:{}:{}]",
-            self.desc.red().bold(),
-            self.src.path.to_str().unwrap(),
-            self.line_number(),
-            self.span.lo,
+        let color = match self.severity {
+            Severity::Warning => Color::BrightYellow,
+            Severity::Fatal => Color::Red,
+            Severity::CodeRed => Color::BrightMagenta,
+        };
+        writeln!(
+            f,
+            "{} {} {}[{}]",
+            "--".bold(),
+            self.desc.color(color).bold(),
+            "------------------------------------------".bold(),
+            self.src.upgrade().unwrap().path.to_str().unwrap().blue()
+        )?;
+        writeln!(f)?;
+        let line = self.line_num().to_string();
+        let front_pad = line.chars().count() + 2;
+        let bar = "|".bold();
+        writeln!(f, "{:>pad$}", bar, pad = front_pad)?;
+        writeln!(
+            f,
+            "{:^pad$}{} {}",
+            line.bold(),
+            bar,
+            self.span_snippet(),
+            pad = front_pad - 1
+        )?;
+        let highlight = format!(
+            "{:pad$}{}",
+            " ",
+            self.underline().color(color).bold(),
+            pad = self.span.lo - self.line_span().lo,
         );
-        writeln!(f, "{}", header.bright_blue());
-        writeln!(f, "");
-        writeln!(f, "{}", self.msg);
-        writeln!(f, "");
-        writeln!(f, "|");
-        writeln!(f, "{}", self.fmt_src_line());
-        writeln!(f, "|");
-        if !self.suggestions.is_empty() {
-            write!(f, "{}", "Hilfe:".yellow().bold().underline());
-            for sug in self.suggestions.iter() {
-                write!(f, " = {}", sug);
-            }
-        }
-        writeln!(f)
+        writeln!(f, "{:>pad$}{}", bar, highlight, pad = front_pad)?;
+        writeln!(
+            f,
+            "{:>pad$} {} {}",
+            bar,
+            "Hilfe:".underline().bold(),
+            self.msg,
+            pad = front_pad
+        )?;
+        write!(f, "")
     }
 }
