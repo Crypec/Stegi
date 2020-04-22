a0011fd880c3d989b49e80c2bcad5d53c2a070fb
Simon Kunz <simon.kunz21@gmail.com>
2020-04-22 14:16:46 +0200

    add very important ascii art

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 #![warn(clippy::pendantic)]
+#![allow(dead_code)]
 use std::path::PathBuf;
 
 #[macro_use]
@@ -15,8 +16,37 @@ extern crate failure;
 
 use self::session::*;
 
+const quote: &str = "Fehler im Code zu suchen ist doppelt so schwer, wie ihn zu schreiben. Wenn sie Code so raffiniert wie moeglich schreiben, sind Sie also per defintion nicht intelligent genug, um ihn zu debuggen. - Brian W. Kernighan";
+
+const stegi_ascii: &str = r#"
+ _________________________________________
+/ Fehler im Code zu suchen ist doppelt so \
+| schwer, wie ihn zu schreiben. Wenn sie  |
+| Code so raffiniert wie moeglich         |
+| schreiben, sind Sie also per defintion  |
+| nicht intelligent genug, um ihn zu      |
+\ debuggen. - Brian W. Kernighan          /
+ -----------------------------------------
+\                             .       .
+ \                           / `.   .' "
+  \                  .---.  <    > <    >  .---.
+   \                 |    \  \ - ~ ~ - /  /    |
+         _____          ..-~             ~-..-~
+        |     |   \~~~\.'                    `./~~~/
+       ---------   \__/                        \__/
+      .'  O    \     /               /       \  "
+     (_____,    `._.'               |         }  \/~~~/
+      `----.          /       }     |        /    \__/
+            `-.      |       /      |       /      `. ,~~|
+                ~-.__|      /_ - ~ ^|      /- _      `..-'
+                     |     /        |     /     ~-.     `-. _  _  _
+                     |_____|        |_____|         ~ - . _ _ _ _
+"#;
+
 fn main() {
-    println!("Hello from the new stegi compiler");
+    println!("################################ Stegi ################################ ");
+    println!("{}", stegi_ascii);
+
     //let mut path = std::env::current_dir().expect("failed to get working dir");
     //path.push("examples/test.st");
     let path = PathBuf::from("examples/test.st");
